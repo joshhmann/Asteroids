3a87a51b70c54e8dc1c186a6bf76a5fca3fd9410
Josh Anderson <joshua.frederick.anderson@gmail.com>
2025-08-05 23:00:14 -0700

    added sprites and additional constants

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +14,8 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                dt = pygame.tick(60)
+            return
         screen.fill(BLACK)
         pygame.display.flip()
         
